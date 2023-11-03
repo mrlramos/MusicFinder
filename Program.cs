775e8316abc0ea28996bed9659fb1376fbfb576c
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-11-03 03:50:09 -0300

    Method ClearFile creation

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!\n");
-
-// Substitua pelo caminho real da pasta e do arquivo TXT.
-string folderPath = @"D:\Downloads\Downloads-FullQuality\Soundeo\PsyTrance-Soundeo";
+﻿// Substitua pelo caminho real da pasta e do arquivo TXT.
+string folderPath = @"D:\Downloads\Downloads-FullQuality\Soundeo\Peak-Soundeo";
 string txtFilePath = @"C:\Users\marlo\Desktop\testepsytrance.txt";
 
 List<string> fileNames = GetFileNames(folderPath);
+
+
+
 List<string> containedFileNames = CheckFilesInTxt(txtFilePath, fileNames);
+
+Console.WriteLine($"Quantidade de arquivos na pasta: {folderPath}");
+Console.WriteLine($"{fileNames.Count}\n\n");
 
 // Imprime os nomes dos arquivos que foram encontrados no arquivo TXT.
 foreach (var fileName in containedFileNames)
 {
-    Console.WriteLine($"{fileName} encontrado no arquivo TXT.");
+    //Console.WriteLine($"{fileName} encontrado no arquivo TXT.");
+
 }
 
 static List<string> GetFileNames(string folderPath)
@@ -22,10 +26,13 @@ static List<string> GetFileNames(string folderPath)
 
     foreach (FileInfo file in files)
     {
-        var tratamentoNome = file.Name.Split("- ");
-        tratamentoNome = tratamentoNome[1].Split(".wav");
-        tratamentoNome = tratamentoNome[0].Split(" (");
-        fileNames.Add(tratamentoNome[0]);
+        if (!file.Name.Contains(".ps1"))
+        {
+            var tratamentoNome = file.Name.Split("- ");
+            tratamentoNome = tratamentoNome[1].Split(".wav");
+            tratamentoNome = tratamentoNome[0].Split(" (");
+            fileNames.Add(tratamentoNome[0]);
+        }
     }
 
     return fileNames;
@@ -34,9 +41,13 @@ static List<string> GetFileNames(string folderPath)
 static List<string> CheckFilesInTxt(string txtFilePath, List<string> fileNames)
 {
     List<string> containedFileNames = new List<string>();
+    List<string> notContainedFileNames = new List<string>();
 
+    int itensEncontrados = 0;
     // Lê o conteúdo do arquivo TXT.
     string txtContent = File.ReadAllText(txtFilePath);
+
+    var text = ClearFile(txtContent);
 
     // Verifica se cada nome de arquivo está contido no conteúdo do arquivo TXT.
     foreach (string fileName in fileNames)
@@ -44,8 +55,60 @@ static List<string> CheckFilesInTxt(string txtFilePath, List<string> fileNames)
         if (txtContent.Contains(fileName))
         {
             containedFileNames.Add(fileName);
+            itensEncontrados++;
+        } else
+        {
+            notContainedFileNames.Add(fileName);
         }
+    }
+
+    Console.WriteLine($"Itens encontrados: {itensEncontrados}\n\n");
+
+    foreach (string fileName in notContainedFileNames)
+    {
+        Console.WriteLine($"Itens não encontrados: {fileName}");
     }
 
     return containedFileNames;
 }
+
+static string ClearFile(string txtContent)
+{
+    string nova = "";
+    var conteudoDoTexto = txtContent.Split("\n");
+
+    string[] totalTracks = new string[182];
+    List<string> nomesCorrigidos = new List<string>();
+
+    for (int i = 0; i < totalTracks.Length; i++)
+    {
+        totalTracks[i] = (i+1)+"\r";
+    }
+
+    int cont = 0;
+    for (int i = 0; i < conteudoDoTexto.Length; i++)
+    {
+        if (conteudoDoTexto[i] == totalTracks[cont]) 
+        {
+            nova += conteudoDoTexto[i+1]+'\n';
+            cont++;
+        }
+    }
+    Console.WriteLine($"Cont: {cont}");
+    Console.WriteLine(nova);
+
+    foreach (string str in nomesCorrigidos)
+    {
+        Console.WriteLine(str);
+    }
+
+    return "";
+}
+
+//for (int z = 0; i < totalTracks.Length; i++)
+//{
+//    if (conteudoDoTexto[z] == totalTracks[z])
+//    {
+//        nomesCorrigidos.Add(conteudoDoTexto[z + 1]);
+//    }
+//}
